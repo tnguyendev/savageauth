bbc7d2408871f31af8386b6331ca4130918cb974
Thao Nguyen <102040536+tnguyendev@users.noreply.github.com>
2022-05-31 00:09:23 -0400

    savageauth

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://thaonguyen:pafW5wI449rrBO5q@tnguyendev.ix5ze.mongodb.net/savageauth?retryWrites=true&w=majority', 
+    'dbName': 'messages'
+};
